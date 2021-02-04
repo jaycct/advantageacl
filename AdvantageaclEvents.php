b19a97c32fd75d5494ef53ac117299a143cbe551
jaysukh <jaysukh.s@communicationcrafts.com>
2021-02-04 11:26:30 +0530

    package install event

---
+++ AdvantageaclEvents.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace jaycct\advantageacl;
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+
+class AdvantageaclEvents extends Component
+{
+   public static function postUpdate(Event $event)
+   {
+        $composer = $event->getComposer();
+		dd($installedPackage);
+   }
+
+    public static function postAutoloadDump(Event $event)
+    {
+        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+		$echo $vendorDir;
+        require $vendorDir . '/autoload.php';
+    }
+
+    public static function postPackageInstall(PackageEvent $event)
+    {
+        $installedPackage = $event->getOperation()->getPackage();
+		dd($installedPackage);
+        // do stuff
+    }
+
+    public static function warmCache(Event $event)
+    {
+        // make cache toasty
+    }
+}
